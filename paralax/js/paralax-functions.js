5e15c13b98151c9cdce6310ca92a52ed6e423b4f
Cornelis Bosua <cornelisbosua@Cornelis-Bosuas-MacBook-Pro.local>
2014-04-27 00:07:22 +1000

    updated by Cornelis

---
+++ paralax/js/paralax-functions.js
@@ -0,0 +1,25 @@
+// created by cjbosua on 8/4/14
+
+$(document).ready(function(){
+		$("#hide").click(function(){
+				$("p").hide();
+		});
+		$("#show").click(function(){
+				$("p").show();
+		});
+		
+		$("#rabout").click(function(){
+				$("p").show();
+		});
+		
+		/*
+		$("#homesection").click(function(){
+				$("#title").hide();
+		});
+		*/
+		
+		var element = document.getElementById('id');
+		element.style.opacity = "0.9";
+		element.style.filter  = 'alpha(opacity=90)';
+});
+// 
