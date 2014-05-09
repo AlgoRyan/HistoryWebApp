d5a7a0fc813cfd94485cf6d21475a9ea807c8a72
bfjin <bfjin07@gmail.com>
2014-05-09 13:50:13 +1000

    more updates

--- members.php
+++ members.php
@@ -1,3 +1,31 @@
+<!DOCTYPE html>
+<html>
+<title> History of CIS </title>
+
+<head>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+<body>
+<nav> <!-- START nav here -->
+	<div id="nav-heading">
+		University of Melbourne
+	</div>
+	<div id="nav-login">
+		<a href="index.html"> home</a>
+		<a href="register.php"> sign up </a>
+		<a href="logout.php"> logout </a>
+	</div>
+	
+	<a href="mod.html"><div id="moderator" > <p>m</p> </div></a>
+	<a href="add-content.html"><div id="add-content" > <p>a</p> </div></a>
+	
+</nav> <!-- END nav here -->
+
+<div class="wrapper" >
+
+<section id="main"> <!-- start Main sectoin -->
+
 <?php 
 
 //Connects to your Database 
