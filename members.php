9524cb2712aa15bb976970875cf70ed2be75551d
MrSohey <MrSohey@users.noreply.github.com>
2014-05-19 14:55:59 +1000

    Update members.php
    Changes to comply to new controller.php

--- members.php
+++ members.php
@@ -28,11 +28,11 @@
     
 			    <?php if(isset($_COOKIE['ID_my_site'])){
 					echo "Welcome " . $_COOKIE['ID_my_site']."!";
-					echo '<li><a href="logout.php"> logout </a></li>';		
+					echo '<li><a href="controller.php?action=logout"> logout </a></li>';		
 				}
 				else{
-					echo '<li><a href="login.php"> log in</a></li>';
-					echo '<li><a href="register.php"> sign up </a></li>';
+					echo '<li><a href="controller.php?action=login"> log in</a></li>';
+					echo '<li><a href="controller.php?action=register"> sign up </a></li>';
 				}
 				?>
 			</ul>
@@ -88,7 +88,7 @@ if(isset($_COOKIE['ID_my_site']))
 		//if the cookie has the wrong password, they are taken to the login page 
 		if ($pass != $info['password']) 
 		{ 			
-			header("Location: login.php"); 
+			header("Location: controller.php?action=login"); 
 		} 
 		//otherwise they are shown the admin area	 
 		else 
@@ -107,7 +107,7 @@ if(isset($_COOKIE['ID_my_site']))
 else 
 //if the cookie does not exist, they are taken to the login screen 
 {			 
-header("Location: login.php"); 
+header("Location: controller.php?action=login"); 
 } 
 
 ?>
